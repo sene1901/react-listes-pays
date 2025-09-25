ff6fc66514090083e598a2158960da602de93424
sene32304@gmail.com <sene32304@gmail.com>
2025-09-25 16:47:20 +0000

    modi-navbvar

--- mon-app/src/components/Navbar.jsx
+++ mon-app/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import logo from "./assets/logo.png";
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark py-3">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#"><img src={logo} alt="Logo" style={{ width: "200px", height: "auto" }}/></a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler bg-white"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -17,28 +17,26 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <a className="nav-link active text-white" aria-current="page" href="#">
               Accueil
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Information</a>
+              <a className="nav-link text-white" href="#">Information</a>
             </li>
+          
             
-           
           </ul>
           <form className="d-flex">
             <input
-              className="form-control me-2"
+              className="form-control me-2 w-50"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
+            
           </form>
         </div>
       </div>
