a9e263b7a1a661b6e8f21bb76246bc2ed5876ee6
sene32304@gmail.com <sene32304@gmail.com>
2025-09-28 17:10:18 +0000

    footer

--- mon-app/src/App.jsx
+++ mon-app/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Pays from './components/Pays'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
     <>
     <Navbar />
+    <Pays />
+    <Footer />
     </>
   )
 }
---
+++ mon-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <footer className="bg-dark text-white text-center py-3 mt-4">
+      <p>© 2025 - GeoApp | Données : REST Countries API</p>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
